498178a11a490d8d986e9d5ad0ca1a636c26e405
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-07 00:07:38 -0300

    add has_secure_password to Reviewer and reviewer data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Book.destroy_all
+Reviewer.destroy_all
 
 Book.create! [
   { name: "Eloquent Ruby" , author: "Russ Olsen" },
@@ -13,3 +14,13 @@ eloquent.notes.create! [
   { title: "Wow", note: "Great book to learn Ruby!" },
   { title: "Funny", note: "Doesn't put you to sleep" }
 ]
+
+reviewers = Reviewer.create! [
+  { name: "Joe", password: "abc123" },
+  { name: "Jim", password: "123abc" }
+]
+
+Book.all.each do |book|
+  book.reviewer = reviewers.sample
+  book.save!
+end
